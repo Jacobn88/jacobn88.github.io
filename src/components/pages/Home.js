8f96e07347c0e115befee8d1b19988d8cff05b86
Jacob Nelson <NelsonJacob1988@gmail.com>
2021-06-29 13:57:49 -0500

    restructures skills page, expands about me section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,8 +14,9 @@ const Home = () => (
         <Image src={me} />
       </Col>
       <Col>
-          <h1>AboutMe</h1>
-          <p id="about">Ambitious Jr. Web Developer and recent graduate of the University of Texas at 
+          <h1>About Me</h1>
+          <p id="about">My name is Jacob Nelson, I'm 32 years old and I live in sunny Austin, Tx. I'm an 
+          ambitious Jr. Web Developer and recent graduate of the University of Texas at 
           Austin's Full-Stack Coding Boot Camp Certificate Program. Front-end focused but familiar with 
           back-end development. Experienced in industry best practices of web and application development 
           from concept to final production.</p>
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,36 +1,41 @@
 import React from "react";
 import "../../styles/style.css";
 import Container from 'react-bootstrap/Container';
+import { Row, Col } from "react-bootstrap";
 
 
 const Skills = () => (
   <Container id="container">
+    <Row>
+      <Col>
+        <div class="skills">
+          <h3>Proficient With</h3>
 
-    <div class="skills">
-      <h3>Proficient With</h3>
-
-      <ul>
-        <li>HTML 5</li>
-        <li>CSS</li>
-        <li>Bootstrap</li>
-        <li>Javascript</li>
-        <li>React</li>
-        <li>Github</li>
-      </ul>
-    </div>
-    <div class="skills">
-      <h3> Experienced With</h3>
-      <ul>
-        <li>Object Oriented Programming</li>
-        <li>Test Driven Development</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>SQL</li>
-        <li>Mongodb</li>
-        <li>APIs, CNDs and much more!</li>
-      </ul>
-    </div>
-
+          <ul>
+            <li>HTML 5</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Github</li>
+          </ul>
+        </div>
+      </Col>
+      <Col>
+        <div class="skills">
+          <h3> Experienced With</h3>
+          <ul>
+            <li>Object Oriented Programming</li>
+            <li>Test Driven Development</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>SQL</li>
+            <li>Mongodb</li>
+            <li>APIs, CDNs and much more!</li>
+          </ul>
+        </div>  
+      </Col>
+    </Row>
   </Container>
 );
 
